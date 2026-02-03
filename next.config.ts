a5e44710e5a7fa880d3c7a90d6349442c01846f8
holgs <holger.ferrero@gmail.com>
2026-02-03 17:06:19 +0100

    Fix: Remove env from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: [],
   },
-  env: {
-    DATABASE_URL: process.env.DATABASE_URL,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  },
 };
 
 export default nextConfig;
